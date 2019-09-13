1837e25825b8a6dde3336427656db97de5e0f9f3
iyan5440 <iyan5440@ISS2V3QKH2.scdsb.pri>
2019-09-13 12:11:19 -0400

    initial commit

--- src/BarChart3.java
+++ src/BarChart3.java
@@ -1,4 +1,7 @@
 
+import TurtleGraphics.*;
+import hsa.Console;
+import java.awt.Color;
 import java.util.Scanner;
 
 
@@ -7,22 +10,34 @@ public class BarChart3 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int x1=0,x2=0,x3=0,x4=0;
+        Pen p = new StandardPen(new SketchPadWindow(1000,600));
+        Color sky;
+        int x1,x2,x3,x4;
+        PenBar pb1 = new PenBar();
+        PenBar pb2 = new PenBar();
+        PenBar pb3 = new PenBar();
+        PenBar pb4 = new PenBar();
+        //PenBar pb1=null,pb2= null,pb3 = null,pb4=null;
+        
         for (int i = 0; i < 4; i++) {
-            System.out.format("Enter value %d",i);
+            System.out.format("Enter value %d from 50 - 450: ",i);
             if(i==0) { 
                 try{
                 x1=s.nextInt();
                 }catch(Exception e){
-                return ;
+                return  ;
                 }
+                        MakeBar(p,pb1,200,x1,Color.blue);
+
             }
             if(i==1) { 
                 try{
                 x2=s.nextInt();
                 }catch(Exception e){
-                return ;
+                return ;   
                 }
+                MakeBar(p,pb2,100,x2,Color.red);
+
             }
             if(i==2) { 
                 try{
@@ -30,6 +45,8 @@ public class BarChart3 {
                 }catch(Exception e){
                 return ;
                 }
+                        MakeBar(p,pb3,0,x3,Color.green);
+
             }
             if(i==3) { 
                 try{
@@ -37,8 +54,23 @@ public class BarChart3 {
                 }catch(Exception e){
                 return ;
                 }
+                        MakeBar(p,pb4,-100,x4,Color.orange);
+                        
+
             }
+            
         }
+
+        
+
+    }
+    public static void MakeBar(Pen p, PenBar pb, int y, int w, Color c){
+        pb.xloc = -500;
+        pb.yloc = y;
+        pb.width = w;
+        pb.col = c;
+        pb.draw(p);
+        
     }
     
 }
