7f51fc66ca2568233db5949027334e4df51dacaf
iyan5440 <iyan5440@ISS2V3QKH2.scdsb.pri>
2019-09-13 12:11:12 -0400

    initial commit

--- src/PenBar.java
+++ src/PenBar.java
@@ -1,19 +1,31 @@
 
+import TurtleGraphics.*;
 import hsa.Console;
 import java.awt.Color;
+import java.util.Scanner;
 
 
 public class PenBar {
     int xloc,width,yloc;
     Color col;
-    public void draw(Console c){
-        if(width >450 || width <50) {
+    public void draw(Pen p){
+        if(width >400 || width <50) {
             System.out.println("ERROR - MUST BE FROM 50 - 450");
             return;
         }
-        c.setColor(col);
-        xloc = 400-width;
-        c.fillRect(xloc, yloc, 50, width);
-        c.drawString("Value: "+  width, xloc-10,yloc);
+        p.setColor(col);
+        p.setWidth(50);
+        //p.setDirection(90);
+        p.up();
+        p.move(xloc,yloc);
+        p.down();
+        p.move(width,yloc);
+        //p.drawString((String.format"Value: ", width, xloc-10,yloc));
+        p.drawString(String.format("         Value: %d ",width));
+        //p.drawString("Value: " + width ,100,100);
+        
+        
     }
+
+    
 }
